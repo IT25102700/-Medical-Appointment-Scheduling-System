90bd821bb8966e04418aa685d1e02984fa2041e1
nathee <natheesan5832@gmail.com>
2026-05-01 20:04:48 +0530

    initial commit

---
+++ src/main/java/com/medapp/medicalapp/MedicalAppointmentSchedulingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.medapp.medicalapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalAppointmentSchedulingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicalAppointmentSchedulingSystemApplication.class, args);
+    }
+
+}
